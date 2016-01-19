14d5cc0c4a224374ed3996c3f739e92cbcb62e65
gibberfish <growler1970@gmail.com>
2016-01-19 21:44:02 +0000

    Added some Javascript / JQuery interactions to the test page

---
+++ src/main/webapp/js/cah.js
@@ -0,0 +1,34 @@
+
+$(function() {
+	$(".my").mouseenter(mouseOverCardInMyHand);
+	$(".my").mouseleave(mouseLeavesCardInMyHand);
+	
+	$(".btn").click(popupModal);
+});
+
+function mouseOverCardInMyHand () {
+	$(this).removeClass("in-background");
+};
+
+function mouseLeavesCardInMyHand () {
+	$(".my").addClass("in-background");
+};
+
+// See http://www.ericmmartin.com/projects/simplemodal/#options
+function popupModal () {
+	$("#popup").modal({overlayClose:true, onOpen: slowOpenDialog});
+
+	setTimeout(function () {
+          $.modal.close();
+    }, 5000);
+
+	return false;
+}
+
+function slowOpenDialog (dialog) {
+	dialog.overlay.fadeIn('slow', function () {
+			dialog.container.slideDown('slow', function () {
+				dialog.data.fadeIn('slow');
+			});
+		});		
+}
