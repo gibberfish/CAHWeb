200b0d46a39c2e39895c5049b06910e9ab4e50a6
gibberfish <growler1970@gmail.com>
2016-02-13 12:02:29 +0000

    Started splitting the cah game from the entry to the game server lobby

---
+++ game.cmd
@@ -0,0 +1,1 @@
+java -jar target\cards-against-humanity-0.1.0.jar
---
+++ src/main/webapp/js/websocket.js
@@ -0,0 +1,47 @@
+
+function connectWebsocket () {
+	var name = $("#name").val();
+    var socket = new SockJS('/gameserver');
+    
+    stompClient = Stomp.over(socket);
+    stompClient.connect({"name" : name}, function(frame) {
+        console.log('Connected: ' + frame);
+        
+        sendCommandToGame("login", name);
+        
+        stompClient.subscribe('/gamestate/gameStateUpdates', function(gameStateActionResponse){
+            var gameStateActionResponseObject = JSON.parse(gameStateActionResponse.body);
+            showGameStateChange(gameStateActionResponseObject.player, gameStateActionResponseObject.command, gameStateActionResponseObject.value);
+        });
+    });
+    $("#connectButton").attr("disabled", true);
+    $("#disconnectButton").removeAttr("disabled");
+    $("#commandButton").removeAttr("disabled");
+}
+
+function disconnectWebsocket () {
+	var name = $("#name").val();
+    if (stompClient != null) {
+    	sendCommandToGame("logout", name);
+        stompClient.disconnect();
+    }
+    $("#connectButton").removeAttr("disabled");
+    $("#disconnectButton").attr("disabled", true);
+    $("#commandButton").attr("disabled", true);
+    console.log("Disconnected");
+}
+        
+function showGameStateChange(player, command, value) {
+	var result = "I have received a message from " + player + ". Command = " + command + ", value = " + value;
+	$("#messages").append ("<p>"+result+"</p>");
+}
+
+function sendCommandToGameFromScreen() {
+    var command = $("#command").val();
+    var value = $("#value").val();
+    sendCommandToGame(command, value);
+}
+
+function sendCommandToGame(command, value) {
+    stompClient.send("/cah/gameserver", {}, JSON.stringify({ 'action': command, 'value': value }));
+}
